88720e51b58990b5f8a1acf723d78c3a15ac73ad
HollowBleach15 <fleshhbi@gmail.com>
2026-05-18 23:44:36 +0300

    fifth commit

--- core/forms.py
+++ core/forms.py
@@ -5,10 +5,11 @@ from .models import Order, Client
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
-        fields = ["full_name", "phone", "email", "address", "is_active"]
+        fields = ["full_name", "phone", "email", "address", "client_type", "is_active"]
         widgets: dict[str, forms.Widget] = {
             **{f: forms.TextInput(attrs={"class": "form-control"})
                for f in ["full_name", "phone", "email", "address"]},
+            "client_type": forms.Select(attrs={"class": "form-select"}),
             "is_active": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
 
