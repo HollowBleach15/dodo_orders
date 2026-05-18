8c95229d5dc2c3cb20fe266f8d3af51e3a5980e4
HollowBleach15 <fleshhbi@gmail.com>
2026-05-19 00:25:11 +0300

    eighth commit

--- app/entrypoint_beat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Waiting for migrations..."
-python manage.py migrate --noinput
-exec celery -A dodo_orders beat -l info \
-  --scheduler django_celery_beat.schedulers:DatabaseScheduler
